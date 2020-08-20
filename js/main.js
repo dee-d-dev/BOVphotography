fec3d820b9aa484b2c0013b631ccae517f2a88b2
dee-d-dev <62015452+dee-d-dev@users.noreply.github.com>
2020-08-20 17:20:23 +0100

    new commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+
+//The close button
+var close = document.getElementById('close');
+
+img.addEventListener('click', Change);
+
+function Change(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    // captionText.innerHTML = this.alt;
+}
+
+close.addEventListener('click', close);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+// function Close(){
+//     modal.style.display = 'none';
+// }
+
